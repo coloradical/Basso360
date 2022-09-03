3496d08e3ee1e74c79ac128d81629e4734bcd303
Megan Byers <megan@designelixir.studio>
2022-09-03 00:58:28 -0600

    mobilizing nav bar

--- index.js
+++ index.js
@@ -20,3 +20,22 @@ $(document).ready(function(){
         }]
     });
 });
+
+var mobileMenuClick = 0;
+function toggleMobileMenu(){
+  mobileMenuClick++;
+  console.log(mobileMenuClick);
+  if (mobileMenuClick % 2 == 0){
+    document.getElementById('main-header').style.height = "150px";
+    document.getElementById('nav-container').style.display = "none";
+    document.getElementById("mobile-menu-img").src = "assets/menu.svg";
+
+  } else {
+    document.getElementById("mobile-menu-img").src = "assets/close.svg";
+    document.getElementById('main-header').style.height = "100vh";
+    document.getElementById('nav-container').style.display = "block";
+    document.getElementById('nav-container').style.height = "65vh";
+    document.getElementById('nav-container').style.marginTop = "5vh";
+    document.getElementsByClassName('nav-container-content').style.display = "flex";
+  }
+}
